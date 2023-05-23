e92b7111b7f84e18a50782edeeb44555a4eab932
zkariaait <zakaria.ait.11@gmail.com>
2023-05-23 21:44:55 +0100

    Search bar

--- pfe_app/lib/constants/routes.dart
+++ pfe_app/lib/constants/routes.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:pfe_app/admin/screens/add_product_screen.dart';
 import 'package:pfe_app/features/auth/screens/login.dart';
-<<<<<<< Updated upstream
-=======
 import 'package:pfe_app/features/product_details/screens/product_details_screen.dart';
 import 'package:pfe_app/features/search/screens/search_screen.dart';
->>>>>>> Stashed changes
 import 'package:pfe_app/features/welcome/welcome.dart';
 import 'package:pfe_app/common/widgets/bottom_bar.dart';
 import 'package:pfe_app/home/screens/category_deals_screen.dart';
 import 'package:pfe_app/home/screens/home_screen.dart';
+import 'package:pfe_app/models/product.dart';
 
 import '../main.dart';
 
@@ -42,9 +40,6 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
         settings: routeSettings,
         builder: (_) => const MyApp(),
       );
-<<<<<<< Updated upstream
-
-=======
     case SearchScreen.routeName:
       var searchQuery = routeSettings.arguments as String;
       return MaterialPageRoute(
@@ -61,7 +56,6 @@ Route<dynamic> generateRoute(RouteSettings routeSettings) {
           product: product,
         ),
       );
->>>>>>> Stashed changes
     case Welcome.routeName:
       return MaterialPageRoute(
         settings: routeSettings,
